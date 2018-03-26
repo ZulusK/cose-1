87601b1f2e18d715ab766a9ed4941ebbebbe0e70
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-03-26 04:49:50 +0300

    add good init test

--- src/Good.py
+++ src/Good.py
@@ -1,6 +1,6 @@
 import re
 import xml.etree.ElementTree as ET
-
+from . import WebSite
 from sty import fg
 
 
@@ -14,28 +14,41 @@ class Good:
         return root
 
     def __normalize_name(self, name):
-        if (self.website.trash):
+        if not name:
+            raise ValueError("Param 'name' is None")
+        if hasattr(self.website, "trash"):
             for regex in self.website.trash:
                 name = re.sub(regex, '', name)
         return name.strip()
 
     def __normalize_URL(self, url):
+        if not url:
+            raise ValueError("Param 'url' is None")
         if url.startswith("/"):
             return self.website.root_url + url
         else:
             return url
 
     def __normalize_price(self, price):
-        return int("".join(price.split()))
+        if not price:
+            raise ValueError("Param 'price' is None")
+        elif isinstance(price, str):
+            return int("".join(price.split()))
+        elif isinstance(price, int):
+            return price
+        else:
+            raise ValueError("Param 'price' in unsupported form. Only string and int are allowed")
 
     def __init__(self, website, name, price, url):
+        if not website:
+            raise Exception("Param 'website' is None")
         self.website = website
         self.name = self.__normalize_name(name)
         self.price = self.__normalize_price(price)
         self.url = self.__normalize_URL(url)
 
     def __str__(self):
-        return "Good '{2}{0!s}{4}', price: {3}${1!s}{4}"\
+        return "Good '{2}{0!s}{4}', price: {3}${1!s}{4}" \
             .format(self.name, self.price, fg.blue, fg.green, fg.rs)
 
     __repr__ = __str__
--- tests/Good_test.py
+++ tests/Good_test.py
@@ -1,3 +1,51 @@
-import src
+from types import SimpleNamespace
 
 import pytest
+
+import src
+
+
+@pytest.mark.parametrize("testCase", [
+    {"website": {"name": "site", "root_url": "https://someURL"},
+     "name": "GoodA",
+     "expectedName": "GoodA",
+     "price": "404",
+     "original_price": 404,
+     "url": "https://someURL/pruduct"},
+    {"website": {"name": "site", "root_url": "https://someURL", "trash": ["мусор", "o*"]},
+     "name": "GoodA мусор   мусор",
+     "expectedName": "GdA",
+     "price": "4 0\n4",
+     "original_price": 404,
+     "url": "/product"},
+])
+def test_Good_normalConstructor(testCase):
+    website = SimpleNamespace(**testCase["website"])
+    g = src.Good(website, testCase["name"], testCase["price"], testCase["url"])
+    assert g.name == testCase["expectedName"].strip()
+    assert g.price == testCase["original_price"]
+    assert g.url.startswith(website.root_url)
+    assert g.website.name == website.name
+
+
+@pytest.mark.parametrize("testCase", [
+    {"name": "GoodA",
+     "price": "404",
+     "url": "https://someURL/pruduct"},
+    {"website": {"name": "site", "root_url": "https://someURL", "trash": ["мусор", "o*"]},
+     "expectedName": "GdA",
+     "price": "4 0\n4",
+     "url": "/product"},
+    {"website": {"name": "site", "root_url": "https://someURL", "trash": ["мусор", "o*"]},
+     "original_price": 404,
+     "url": "/product"},
+    {"website": {"name": "site", "root_url": "https://someURL", "trash": ["мусор", "o*"]},
+     "name":"abs",
+     "price": "404"},
+])
+def test_Good_invalidConstructor(testCase):
+    website = None
+    if ("website" in testCase):
+        website = SimpleNamespace(**testCase["website"])
+    with pytest.raises(Exception):
+        src.Good(website, testCase["name"], testCase["price"], testCase["url"])
